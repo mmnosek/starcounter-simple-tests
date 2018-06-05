c9dbd3004901f1cd47a0653a66787890a91ac78e
Michal Nosek <mmnosek@gmail.com>
2018-06-05 15:30:27 +0200

    Change user test to GET

--- SCNova/Controllers/UserController.cs
+++ SCNova/Controllers/UserController.cs
@@ -1,22 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NovaSimplePerfTests.Models;
 using Starcounter.Nova;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace NovaSimplePerfTests.Controllers
 {
     public class UserController : Controller
     {
-        // POST: /User/Add
-        [HttpPost]
-        public async Task<IActionResult> Add(string username, string email, string password)
+        // GET: /User/Add
+        [HttpGet("User/Add/{username}/{password}")]
+        public async Task<IActionResult> Add(string username, string password)
         {
             User user = null;
 
@@ -24,14 +19,11 @@ namespace NovaSimplePerfTests.Controllers
             {
                 user = Db.Insert<User>();
                 user.UserName = username;
-                user.Email = email;
             });
-           
             Db.Transact(() =>
             {
                 user.PasswordHash = Convert.ToBase64String(Encoding.UTF8.GetBytes(password));
             });
-
 
             return Content("Added");
         }
